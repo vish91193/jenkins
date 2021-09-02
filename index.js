add06fee44bde8544e2f15f37b52e996a6d16873
vish91193 <vishal.sviet@gmail.com>
2021-09-03 00:14:03 +0530

    version2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V11152!')
+  res.send('CICD App V2!')
 })
 
 app.get('/status', (req, res) => {
